9f1a37db27b73f8cced9dd43d4614fa313b3af5f
Rian Engracia <engraciarian@gmail.com>
2023-12-28 21:24:20 +0800

    Add GetSectionNews to interface

--- src/Application/News/INewsRepository.cs
+++ src/Application/News/INewsRepository.cs
@@ -1,9 +1,11 @@
 namespace gma_news_api.Application.News;
 using System.Threading.Tasks;
 using Entities;
-using gma_news_api.Application.News.Queries.GetNews;
+using gma_news_api.Application.News.Queries.GetSectionNews;
+using gma_news_api.Application.News.Queries.GetSportsNews;
 
 public interface INewsRepository
 {
     Task<List<News>> GetNews(GetNewsQuery request, CancellationToken cancellationToken);
+    Task<List<News>> GetSectionNews(GetSectionNewsQuery request, CancellationToken cancellationToken);
 }
