b923a6ca301fb8765e9f8dfe148567489afe6d03
Rian Engracia <engraciarian@gmail.com>
2024-01-09 21:05:03 +0800

    Remove general news endpoint

--- src/Presentation/Endpoints/NewsEndpoints.cs
+++ src/Presentation/Endpoints/NewsEndpoints.cs
@@ -14,58 +14,52 @@ public static class NewsEndpoints
             .WithTags("news")
             .WithOpenApi();
 
-        _ = root.MapGet("/", GetNews)
-            .Produces<List<Entities.News>>()
-            .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all news")
-            .WithDescription("\n    GET /news");
-
         _ = root.MapGet("/topstories", GetTopStoriesNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all top stories news")
+            .WithSummary("Lookup all top stories news. Select one or more sub sections (Ulat Filipino, Nation, World, Regions, Metro, Special Reports). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/topstories");
 
         _ = root.MapGet("/money", GetMoneyNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all top stories news")
+            .WithSummary("Lookup all money news. Select one or more sub sections (Economy, Personal Finance, Companies, Monitoring). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/money");
 
         _ = root.MapGet("/sports", GetSportsNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all sports news")
+            .WithSummary("Lookup all sports news. Select one or more sub sections (Volleyball, Basketball, Boxing, Football). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/sports");
 
         _ = root.MapGet("/pinoyabroad", GetPinoyAbroadNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all sports news")
+            .WithSummary("Lookup all pinoy abroad news. Select one or more sub sections (Dispatch, Pinoy Achievers, Immigration Guide). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/pinoyabroad");
 
         _ = root.MapGet("/scitech", GetScitechNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all science and technology news")
+            .WithSummary("Lookup all science and technology news. Select one or more sub sections (Weather, Science and Research, Boxing, -Technology, Gadgets and Gaming-). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/scitech");
 
         _ = root.MapGet("/showbiz", GetShowbizNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all showbiz news")
+            .WithSummary("Lookup all showbiz news. Select one or more sub sections (Pep, Chika Minute, Showbiz Abroad). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/showbiz");
 
         _ = root.MapGet("/lifestyle", GetLifestyleNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all lifestyle news")
+            .WithSummary("Lookup all lifestyle news. Select one or more sub sections (Family and Relationships, Travel, Food, Art and Culture, Health and Wellness, Shopping and Fashion, Hobbies and Activities). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/lifestyle");
 
         _ = root.MapGet("/opinion", GetOpinionNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all opinion news")
+            .WithSummary("Lookup all opinion news. Select one or more sub sections (News Hardcore). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/opinion");
 
         _ = root.MapGet("/hashtag", GetHashtagNews)
@@ -77,7 +71,7 @@ public static class NewsEndpoints
         _ = root.MapGet("/serbisyopubliko", GetSerbisyoPublikoNews)
             .Produces<List<Entities.News>>()
             .ProducesProblem(StatusCodes.Status500InternalServerError)
-            .WithSummary("Lookup all serbisyo publiko news")
+            .WithSummary("Lookup all serbisyo publiko news. Select one or more sub sections (Walang Pasok, Transportation, Missing Persons). Automatically choose all sub-sections if none are explicitly selected.")
             .WithDescription("\n    GET /news/serbisyopubliko");
 
         _ = root.MapGet("/cbb", GetCbbNews)
@@ -87,20 +81,6 @@ public static class NewsEndpoints
             .WithDescription("\n    GET /news/cbb");
 
         return app;
-    }
-
-    public static async Task<IResult> GetNews([FromQuery] string[]? section,
-    [FromQuery] string[]? subSection, int page, int pageSize, string? sortColumn, string? sortOrder, IMediator mediator)
-    {
-        try
-        {
-            return Results.Ok(await mediator.Send(new Queries.GetSportsNews.GetNewsQuery(section, subSection)));
-        }
-        catch (Exception ex)
-        {
-            return Results.Problem(ex.StackTrace, ex.Message, StatusCodes.Status500InternalServerError);
-
-        }
     }
     public static async Task<IResult> GetTopStoriesNews([FromQuery] string[]? subSection, int page, int pageSize, string? sortColumn, string? sortOrder, IMediator mediator)
     {
@@ -150,7 +130,16 @@ public static class NewsEndpoints
     {
         try
         {
-            return Results.Ok(await mediator.Send(new Queries.GetSectionNews.GetSectionNewsQuery(section, subSection, page, pageSize, sortColumn, sortOrder)));
+            return Results.Ok(await mediator.Send(new Queries.GetSectionNews.GetSectionNewsQuery
+            {
+                Section = section,
+                SubSection = subSection,
+                Page = page,
+                PageSize = pageSize,
+                SortColumn = sortColumn,
+                SortOrder = sortOrder
+            }));
+            //return Results.Ok(await mediator.Send(new Queries.GetSectionNews.GetSectionNewsQuery(section, subSection, page, pageSize, sortColumn, sortOrder)));
         }
         catch (Exception ex)
         {
