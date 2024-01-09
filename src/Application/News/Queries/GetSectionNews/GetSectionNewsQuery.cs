37d8e59ff4d96800238fd65d8fb97670447c1f19
Rian Engracia <engraciarian@gmail.com>
2024-01-09 21:16:38 +0800

    Convert this class to parameterless

--- src/Application/News/Queries/GetSectionNews/GetSectionNewsQuery.cs
+++ src/Application/News/Queries/GetSectionNews/GetSectionNewsQuery.cs
@@ -1,13 +1,28 @@
+//namespace gma_news_api.Application.News.Queries.GetSectionNews;
+
+//using gma_news_api.Application.News.Entities;
+//using MediatR;
+//public class GetSectionNewsQuery(string? section, string[]? subSection, int page, int pageSize, string? sortColumn, string? sortOrder) : IRequest<List<News>>
+//{
+//    public string? Section { get; set; } = section;
+//    public string[]? SubSection { get; set; } = subSection;
+//    public int Page { get; set; } = page;
+//    public int PageSize { get; set; } = pageSize;
+//    public string? SortColumn { get; set; } = sortColumn;
+//    public string? SortOrder { get; set; } = sortOrder;
+//}
+
 namespace gma_news_api.Application.News.Queries.GetSectionNews;
 
 using gma_news_api.Application.News.Entities;
 using MediatR;
-public class GetSectionNewsQuery(string? section, string[]? subSection, int page, int pageSize, string? sortColumn, string? sortOrder) : IRequest<List<News>>
+public class GetSectionNewsQuery : IRequest<List<News>>
 {
-    public string? Section { get; set; } = section;
-    public string[]? SubSection { get; set; } = subSection;
-    public int Page { get; set; } = page;
-    public int PageSize { get; set; } = pageSize;
-    public string? SortColumn { get; set; } = sortColumn;
-    public string? SortOrder { get; set; } = sortOrder;
+    public required string Section { get; set; }
+    public string[]? SubSection { get; set; }
+    public required int Page { get; set; }
+    public required int PageSize { get; set; }
+    public string? SortColumn { get; set; }
+    public string? SortOrder { get; set; }
 }
+
